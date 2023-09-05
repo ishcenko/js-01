c51d3fe40571e6b7a0980d9607fabf8f5115282d
ishcenko <a_ishcenko@i.ua>
2023-09-05 19:30:16 +0300

    lesson-01-2 end

--- scripts.js
+++ scripts.js
@@ -46,45 +46,29 @@
 
 //* Example 4 - Вкладені розгалуження
 // Напиши скрипт, який порівнює числа у змінних a та b. Якщо обидва значення більше 100, то виведи в консоль максимальне з них. В протилежному випадку у консолі повинна бути сума значення b та числа 512.
-const a = 120;
-const b = 99;
-if (a >= 100 && b >= 100) {
-  //   if (a > b) {
-  //     console.log(a);
-  //   } else {
-  //     console.log(b + 512);
-  //   }
-  //   console.log("Yes");
-  console.log(Math.max(a, b));
-} else {
-  console.log(b + 512);
-}
-
-let result = a > 100 && b > 100 ? (a > b ? a : b) : b + 512;
-console.log(result);
-
+//
 //* Example 10 - Дедлайн здачі проекту (switch)
 // Виконай рефакторинг коду задачі номер 5 використовуючи switch.
-const daysUntil = 0;
+// const daysUntil = 0;
 
-switch (daysUntil) {
-  case 0: {
-    console.log("Now");
-    break;
-  }
-  case 1: {
-    console.log("Now+1");
-    break;
-  }
-  case 2: {
-    console.log("Now+2");
+// switch (daysUntil) {
+//   case 0: {
+//     console.log("Now");
+//     break;
+//   }
+//   case 1: {
+//     console.log("Now+1");
+//     break;
+//   }
+//   case 2: {
+//     console.log("Now+2");
 
-    break;
-  }
-  default: {
-    console.log("Now++");
-  }
-}
+//     break;
+//   }
+//   default: {
+//     console.log("Now++");
+//   }
+// }
 
 // if (daysUntil === 0) {
 //   console.log("Now");
@@ -95,3 +79,80 @@ switch (daysUntil) {
 // } else {
 //   console.log("Now++");
 // }
+
+//* Example 11 - Цикл for
+// Напиши цикл for, який виводить у консоль браузера числа за зростанням від min до max, але тільки якщо число кратне 5.
+
+// const max = 100;
+// const min = 20;
+
+// for (let i = min; i <= max; i += 1) {
+//   if (i % 5 !== 0) continue;
+//   console.log(i);
+// }
+
+// for (let i = min; i <= max; i += 1) {
+//   console.log(i);
+//   if (i % 5 !== 0) continue;
+// }
+
+// for (let i = min; i <= max; i += 1) {
+//   if (i % 5 !== 0) break;
+//   console.log(i);
+// }
+// let total = 0;
+
+// for (let i = min; i <= max; i += 1) {
+//   total += i;
+// }
+// console.log(total);
+
+// for (let i = max; i >= min; i -= 1) {
+//   if (i % 5 === 0) {
+//     console.log(i);
+//   }
+//   // console.log(i);
+// }
+
+// for (let i = min; i <= max; i += 1) {
+//   if (i % 5 === 0) {
+//     console.log(i);
+//   }
+//   // console.log(i);
+// }
+
+// for (let i = min; i <= max; i += 5) {
+//   console.log(i);
+// }
+
+// for (let i = 2; i <= 10; i += 2) {
+//   console.log(i);
+// }
+
+//* Example 12 - Введення користувача та розгалуження
+// Напиши скрипт, який питатиме логін за допомогою prompt та логувати результат у консоль браузера.
+
+// Якщо відвідувач вводить "Адмін", то prompt запитує пароль
+// Якщо нічого не введено або натиснуто клавішу Esc - вивести рядок "Скасовано"
+// В іншому випадку вивести рядок "Я вас не знаю"
+// Пароль перевіряти так:
+
+// Якщо введено пароль "Я адмін", то вивести рядок "Здрастуйте!"
+// Інакше виводити рядок "Невірний пароль"
+
+const lodin = prompt("Login")?.trim();
+// console.log(lodin);
+
+if (lodin === "123456") {
+  const password = prompt("Login");
+  if (password === "123456") {
+    console.log("Hi!");
+  } else {
+    console.log(" No password");
+  }
+} else if (!lodin) {
+  // else if (lodin === "" || lodin === undefined)
+  console.log("Скасвано");
+} else {
+  console.log("Я вас не знаю");
+}
