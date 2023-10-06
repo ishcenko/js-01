1758e4a759f9eae3449ee59410976a7948753d8a
ishcenko <a_ishcenko@i.ua>
2023-10-06 16:47:02 +0300

    До перерви lesson-03-2

---
+++ scripts.js
@@ -0,0 +1,54 @@
+//* Example 1 - Деструктуризація
+// Перепиши функцію так, щоб вона приймала один об'єкт параметрів замість набору незалежних аргументів.
+// function calcBMI({ weight, height }) {
+//   const numericWeight = Number(weight.replace(",", "."));
+//   const numericHeight = Number(height.replace(",", "."));
+//   return Number((numericWeight / numericHeight ** 2).toFixed(1));
+// }
+// //Було;
+// // console.log(calcBMI("88,3", "1.75"));
+// // console.log(calcBMI("68,3", "1.65"));
+// // console.log(calcBMI("118,3", "1.95"));
+
+// //Очікується;
+// console.log(
+//   calcBMI({
+//     height: "1.75",
+//     weight: "88,3",
+//   })
+// );
+// console.log(
+//   calcBMI({
+//     weight: "68,3",
+//     height: "1.65",
+//   })
+// );
+// console.log(
+//   calcBMI({
+//     weight: "118,3",
+//     height: "1.95",
+//   })
+// );
+//!==============================================
+//* Example 2 - Деструктуризація
+// Перепиши функцію так, щоб вона приймала один об'єкт параметрів замість набору незалежних аргументів.
+
+// function printContactsInfo({ names, phones }) {
+//   const nameList = names.split(",");
+//   const phoneList = phones.split(",");
+//   for (let i = 0; i < nameList.length; i += 1) {
+//     console.log(`${nameList[i]}: ${phoneList[i]}`);
+//   }
+// }
+
+// // Було
+// // printContactsInfo(
+// //   'Jacob,William,Solomon,Artemis',
+// //   '89001234567,89001112233,890055566377,890055566300',
+// // );
+
+// // Очікується
+// printContactsInfo({
+//   names: "Jacob,William,Solomon,Artemis",
+//   phones: "89001234567,89001112233,890055566377,890055566300",
+// });
