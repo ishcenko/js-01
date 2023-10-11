2641fb9f409a581c4160c40e3f84f9f0c8cdff43
ishcenko <a_ishcenko@i.ua>
2023-10-11 18:26:58 +0300

    lesson 5/2 (1:02)

---
+++ scripts.js
@@ -0,0 +1,29 @@
+// class User {
+//   static calcBirthDate(age) {
+//     return 2023 - age;
+//   }
+//   constructor(userName, userAge) {
+//     // console.log("Welcome");
+//     // console.log(this);
+//     // console.log(userName, userAge);
+//     this.name = userName;
+//     this.age = userAge;
+//   }
+//   greeting() {
+//     console.log(
+//       `Hello,${this.name}! You was born in ${User.calcBirthDate(this.age)}`
+//     );
+//   }
+// }
+
+// const user = new User("Andrii", 34);
+// // console.log(user);
+// user.greeting();
+// console.log(User.calcBirthDate(73));
+// console.log(User.calcBirthDate(47));
+
+// console.dir(user);
+
+// const arr = [1, 2, 3];
+// console.dir(arr);
+//!=======================
