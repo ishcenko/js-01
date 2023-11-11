a70615411c4912214f8e75bd4943b2e7fe677cb4
ishcenko <a_ishcenko@i.ua>
2023-11-11 11:14:11 +0200

    the end

--- scripts.js
+++ scripts.js
@@ -53,3 +53,44 @@ window.addEventListener(
     refs.otuputResizeThrottled.textContent = eventResizeCounter.throttle;
   }, 1000)
 );
+
+const nameInput = document.getElementById("nameInput");
+nameInput.addEventListener(
+  "input",
+  _.debounce(
+    (event) => {
+      console.log(event.target.value);
+    },
+    1000,
+    {
+      leading: true,
+      trailing: true,
+    }
+  )
+);
+//!==============================================
+
+const signInForm = document.getElementById("signInForm");
+
+const classes = {
+  wrong: "wrong",
+  correct: "correct",
+};
+
+signInForm.addEventListener(
+  "input",
+  _.debounce(({ target }) => {
+    const { value } = target;
+    // console.log(target.dataset); //value
+    if (
+      value.length < target.dataset.minLength ||
+      value.length > target.dataset.maxLength
+    ) {
+      target.classList.add(classes.wrong);
+      target.classList.remove(classes.correct);
+    } else {
+      target.classList.add(classes.correct);
+      target.classList.remove(classes.wrong);
+    }
+  }, 1000)
+);
