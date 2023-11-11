83ec8b5d1e6099b6cfe85b75d9958de8e2a9d5b3
ishcenko <a_ishcenko@i.ua>
2023-11-11 13:27:28 +0200

    не розібрався з опасіті 0/1

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// document.querySelector(".more-button").addEventListener("click", function () {
+//   document.querySelector(".list-container").classList.toggle("active");
+// });
+document.querySelector(".more-button").addEventListener("click", function () {
+  document.querySelector(".list-container").classList.toggle("active");
+});
