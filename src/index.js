98b6100380b2d6b5b06de532bee76e3a7f2d6b13
ishcenko <a_ishcenko@i.ua>
2023-11-14 15:35:37 +0200

    the end 08-01

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./css/main.css";
+import "./css/modal.css";
+import "./images/hamer.jpg";
+import { mult, sum } from "./functions";
+console.log("Hello world");
+console.log(sum(6, 4));
+console.log(mult(4, 55));
